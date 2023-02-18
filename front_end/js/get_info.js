3daf7af04fce97ee9df4b454052bc5d4bb57a660
Eirik Berget Haukeland <eirik@haukeland.name>
2023-02-18 14:22:20 +0100

    feching recipe info from api

---
+++ front_end/js/get_info.js
@@ -0,0 +1,16 @@
+const carusell_cards = document.querySelector('#carusell_content');
+fetch("https://examapi.ebh.fyi/wp-json/wp/v2/wprm_recipe")
+  .then(response => response.json())
+  .then(response => {
+    console.log(response);
+    response.forEach(recipe => {
+      const card = document.createElement("a");
+      card.innerHTML = `
+                        <img src="img/test-imgs/smørgrøt" alt="butter porridge">
+                        <h3>${recipe.title.rendered}</h3>
+                        <span><img src="img/icons/time.svg" alt="time">30 min</span>`
+      card.setAttribute("href", `aricle.html?id=${recipe.id}`)
+      card.classList.add("card");
+      carusell_cards.appendChild(card);
+    })
+  })
