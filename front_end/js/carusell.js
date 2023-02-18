900195b5809455e69ffa2c7bf21d3fa594d8e787
Eirik Berget Haukeland <eirik@haukeland.name>
2023-02-18 20:09:32 +0100

    miner changes to changeHidden and made carusell move

--- front_end/js/carusell.js
+++ front_end/js/carusell.js
@@ -1,14 +1,71 @@
 const next_btn = document.querySelector("#carusell_next_btn");
 const previus_btn = document.querySelector("#carusell_previus_btn");
 const carusell_cards = document.querySelector('#carusell_content');
+console.table(carusell_cards);
 
-console.log(carusell_cards.childNodes);
+const changeHidden = (elements) => {
+  elements.forEach((el, index) => {
+    if (window.innerWidth <= 900) {
+      el.removeAttribute("hidden");
 
-console.log(carusell_cards.childNodes.filter(t => t.innerText !== "text"));
+    } else if (window.innerWidth > 900 && window.innerWidth <= 1200) {
+      if (index < 3) {
+        el.removeAttribute("hidden");
+      } else {
+        el.setAttribute("hidden", true);
+      }
 
-// const test = carusell_cards.childNodes.filter(item => item !== "#text");
-// console.log(test)
-// previus_btn.onclick(() => console.log("previus"));
+    } else if (window.innerWidth >= 1200) {
+      if (index < 4) {
+        el.removeAttribute("hidden");
+      } else {
+        el.setAttribute("hidden", true);
+      }
+    }
+  })
+}
 
+changeHidden([...carusell_cards.children]);
+window.onresize = () => changeHidden([...carusell_cards.children]);
 
-console.log(`${next_btn}, ${previus_btn}, ${carusell_cards}`);
+const move_items = (list, dir, amount) => {
+  console.log(list.slice(0, 3))
+  const cards = []
+  for (let i = 0; i < amount; i++) {
+    if (dir === "next") {
+      cards.push(list.shift())
+    } else if (dir === "previus") {
+      cards.unshift(list.pop())
+    }
+  }
+    if (dir === "next") {
+    return [...list, ...cards]
+  } else if (dir === "previus") {
+    return[...cards, ...list]
+  }
+}
+
+const move_carusell = (nodelist, dir) => {
+  let nList
+  if (dir === "next" && window.innerWidth >= 900 && window.innerWidth <= 1200) {
+    nList = move_items(nodelist, "next", 3);
+  } else if (dir === "previus" && window.innerWidth >= 900 && window.innerWidth <= 1200) {
+    nList = move_items(nodelist, "previus", 3);
+  } else if (dir === "next" && window.innerWidth >= 1200) {
+    nList = move_items(nodelist, "next", 4);
+  } else if (dir === "previus" && window.innerWidth >= 1200) {
+    nList = move_items(nodelist, "previus", 4);
+  }
+  carusell_cards.innerHTML = "";
+  nList.forEach(el => carusell_cards.appendChild(el))
+}
+
+previus_btn.onclick = () => {
+  move_carusell([...carusell_cards.children], "previus");
+  changeHidden([...carusell_cards.children]);
+};
+
+next_btn.onclick = () => {
+  move_carusell([...carusell_cards.children], "next");
+  changeHidden([...carusell_cards.children]);
+};
