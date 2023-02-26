22adcdbf8abe44b92a56c665956aec795d02a328
Eirik Berget Haukeland <eirik@haukeland.name>
2023-02-26 18:50:23 +0100

    added time to cards

--- front_end/js/makeCard.js
+++ front_end/js/makeCard.js
@@ -1,9 +1,14 @@
 export default (recipeInfo) => {
   const card = document.createElement("a");
+  let time = 5
+  console.log(recipeInfo.recipe.total_time)
+  if (parseInt(recipeInfo.recipe.total_time, 10) > 5) {
+    time = recipeInfo.recipe.total_time;
+  }
   card.innerHTML = `
   <img src="${recipeInfo.recipe.image_url}" alt="butter porridge">
   <h3>${recipeInfo.title.rendered}</h3>
-  <span><img src="img/icons/time.svg" alt="time">30 min</span>` // todo: add time form api-call
+  <span><img src="img/icons/time.svg" alt="time">${time} min</span>` // todo: add time form api-call
   card.setAttribute("href", `aricle.html?id=${recipeInfo.id}`)
   card.setAttribute("data-tags", `${recipeInfo.wprm_keyword}`)
   card.classList.add("card");
